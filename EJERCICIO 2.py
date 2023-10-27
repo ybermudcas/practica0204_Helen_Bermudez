e27aa063b79b3f41852aaa7c71329cc037cd2d62
ybermudcas <ybermudcas@educacion.navarra.es>
2023-10-27 22:27:24 +0200

    EJERCICIO 2

---
+++ EJERCICIO 2.py
@@ -0,0 +1,9 @@
+dividendo = float(input("Escribe un número"))
+divisor = float(input("Escribe un segundo número"))
+
+if divisor == 0:
+    print("Error, el divisor no puede ser 0")
+
+else:
+    print("El resultado de la división es" , dividendo/divisor)
+
